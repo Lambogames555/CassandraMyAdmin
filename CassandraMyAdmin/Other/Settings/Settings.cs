87cf502ba9e56d8e4ffffeef89d9dafc22faab5d
Philipp Stein <22456531+Lambogames555@users.noreply.github.com>
2023-05-13 10:30:21 +0200

    Settings system reworked.

--- CassandraMyAdmin/Other/Settings/Settings.cs
+++ CassandraMyAdmin/Other/Settings/Settings.cs
@@ -1,39 +1,51 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace CassandraMyAdmin.Other.Settings;
 
 public class Settings
 {
+    [DefaultValue("1")]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public string configVersion { get; set; } = "1";
+    public string configVersion { get; set; }
 
+    [DefaultValue("127.0.0.1")]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public string cassandraIp { get; set; } = "127.0.0.1";
+    public string cassandraIp { get; set; }
 
+    [DefaultValue(9042)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int cassandraPort { get; set; } = 9042;
+    public int cassandraPort { get; set; }
     
+    [DefaultValue(false)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool onlySuperUserLogin { get; set; } = false;
+    public bool onlySuperUserLogin { get; set; }
     
+    [DefaultValue(true)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool hideSystemKeySpaces { get; set; } = true;
-    
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool countKeyspaceTables { get; set; } = true;
-    
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool countTableRows { get; set; } = false;
-    
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int captchaMode { get; set; } = 2; //0 = disabled, 1 = always, 2 = only on suspicious logins
-    
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int captchaDifficulty { get; set; } = 4;
+    public bool hideSystemKeySpaces { get; set; }
 
+    [DefaultValue(true)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool fail2Ban { get; set; } = true;
+    public bool countKeyspaceTables { get; set; }
+
+    [DefaultValue(false)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+    public bool countTableRows { get; set; }
     
+    [DefaultValue(2)] //0 = disabled, 1 = always, 2 = only on suspicious logins
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool silentBan { get; set; } = true;
+    public int captchaMode { get; set; }
+    
+    [DefaultValue(4)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+    public int captchaDifficulty { get; set; }
+
+    [DefaultValue(true)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool fail2Ban { get; set; }
+    
+    [DefaultValue(true)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool silentBan { get; set; }
 }
