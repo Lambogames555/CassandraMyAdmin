acec9e8c06816111fffae76e9a0e7e474f41b453
Philipp Stein <22456531+Lambogames555@users.noreply.github.com>
2023-05-13 12:01:13 +0200

    Fixed a problem that the config system created "Null" values.

--- CassandraMyAdmin/Other/Settings/Settings.cs
+++ CassandraMyAdmin/Other/Settings/Settings.cs
@@ -6,46 +6,46 @@ namespace CassandraMyAdmin.Other.Settings;
 public class Settings
 {
     [DefaultValue("1")]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public string configVersion { get; set; }
-
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public string configVersion { get; set; } = "1";
+    
     [DefaultValue("127.0.0.1")]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public string cassandraIp { get; set; }
-
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public string cassandraIp { get; set; } = "127.0.0.1";
+    
     [DefaultValue(9042)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int cassandraPort { get; set; }
-    
-    [DefaultValue(false)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool onlySuperUserLogin { get; set; }
-    
-    [DefaultValue(true)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool hideSystemKeySpaces { get; set; }
-
-    [DefaultValue(true)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool countKeyspaceTables { get; set; }
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public int cassandraPort { get; set; } = 9042;
 
     [DefaultValue(false)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public bool countTableRows { get; set; }
-    
-    [DefaultValue(2)] //0 = disabled, 1 = always, 2 = only on suspicious logins
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int captchaMode { get; set; }
-    
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool onlySuperUserLogin { get; set; } = false;
+
+    [DefaultValue(true)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool hideSystemKeySpaces { get; set; } = true;
+
+    [DefaultValue(true)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool countKeyspaceTables { get; set; } = true;
+
+    [DefaultValue(false)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public bool countTableRows { get; set; } = false;
+
+    [DefaultValue(2)]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public int captchaMode { get; set; } = 2; //0 = disabled, 1 = always, 2 = only on suspicious logins
+
     [DefaultValue(4)]
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
-    public int captchaDifficulty { get; set; }
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
+    public int captchaDifficulty { get; set; } = 4;
 
     [DefaultValue(true)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
-    public bool fail2Ban { get; set; }
-    
+    public bool fail2Ban { get; set; } = true;
+
     [DefaultValue(true)]
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate, NullValueHandling = NullValueHandling.Ignore)]
-    public bool silentBan { get; set; }
+    public bool silentBan { get; set; } = true;
 }
