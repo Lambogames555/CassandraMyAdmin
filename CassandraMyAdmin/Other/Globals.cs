a2ffc47db74040c10635ac31dc1f4ab56bf5a3e0
Philipp Stein <22456531+Lambogames555@users.noreply.github.com>
2023-05-13 10:56:04 +0200

    TODO removed and version number changed.

--- CassandraMyAdmin/Other/Globals.cs
+++ CassandraMyAdmin/Other/Globals.cs
@@ -6,12 +6,11 @@ namespace CassandraMyAdmin.Other;
 
 public class Globals
 {
-    internal const string Version = "0.2.0";
+    internal const string Version = "0.4.0";
 
     internal static readonly GitHubApiHelper GitHubApiHelper = new("Lambogames555", "CassandraMyAdmin", new MemoryCache(new MemoryCacheOptions()));
     
-    //TODO auto logout of sessions / delete sessions on logout
-    internal static Dictionary<string, CassandraManager> Sessions = new();
+    internal static Dictionary<string, (CassandraManager, DateTime)> Sessions = new();
 
     internal static Settings.Settings Settings = null!;
 }
