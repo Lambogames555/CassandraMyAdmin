fed42eaf8b5429710b3b1bc079f0f499aed44c8e
Philipp Stein <22456531+Lambogames555@users.noreply.github.com>
2023-05-13 10:51:46 +0200

    Session manager added to delete old sessions.

---
+++ CassandraMyAdmin/Other/Manager/SessionsManager.cs
@@ -0,0 +1,22 @@
+﻿namespace CassandraMyAdmin.Other.Manager;
+
+public class SessionsManager
+{
+    private static readonly TimeSpan cleanupInterval = TimeSpan.FromMinutes(5);
+
+    // Start the cleaner timer
+    private static readonly Timer cleanerTimer = new Timer(CleanUp, null, cleanupInterval, cleanupInterval);
+
+    private static void CleanUp(object state)
+    {
+        // Get the current timestamp
+        var now = DateTime.Now;
+    
+        var expiredSessions = Globals.Sessions.Where(session => session.Value.Item2 <= now).Select(session => session.Key).ToList();
+
+        foreach (var expiredSession in expiredSessions)
+        {
+            Globals.Sessions.Remove(expiredSession);
+        }
+    }
+}
